bfc735cb724eefa97eecdce1114b764fb474003f
vscholl <victoria.scholl@colorado.edu>
2020-10-22 22:00:27 -0600

    added CatterPlots parabola rainbow with Nyan Cat

--- amusing_R_packages.R
+++ amusing_R_packages.R
@@ -53,10 +53,21 @@ for(i in 1:52){
 # My favorites are definitely Lil Jon #36 and #37 
 BRRR::skrrrahh(36)
 BRRR::skrrrahh(37)
+BRRR::skrrrahh(23)
 
 
 
 # Catterplot --------------------------------------------------------------
 
+# https://github.com/Gibbsdavidl/CatterPlots
+
+# install Catterplot 
+install_github("Gibbsdavidl/CatterPlots")
+library(CatterPlots)
+
+x <- -10:10
+y <- -x^2 + 10
+# make a rainbow parabola with Nyan Cat
+rainbowCats(x, y, yspread=0.05, xspread=0.05, ptsize=2, catshiftx=0.5, catshifty=-0.2, canvas=c(-0.5,1.5,-1,1.5))
 
 
